30b5fc4e27ddef9cf5acc962733bcc50dde943fe
sinha97 <vk9633698@gmail.com>
2022-11-20 11:20:52 +0530

    chainging chat name

--- public/js/common.js
+++ public/js/common.js
@@ -603,3 +603,20 @@ function updateSelectedUsershtml() {
   $(".selectedUser").remove();
   $("#selectedUsers").prepend(elements);
 }
+
+function getChatName(chatData) {
+  var chatName = chatData.chatName;
+  if (!chatName) {
+    var otherChatUsers = getOtherChatUsers(chatData.users);
+    var namesArray = otherChatUsers.map(user => user.firstName + " " + user.lastName);
+    chatName = namesArray.join(", ")
+  }
+
+  return chatName;
+}
+
+function getOtherChatUsers(users) {
+  if (users.length == 1) return users;
+
+  return users.filter(user => user._id != userLoggedIn._id);
+}
